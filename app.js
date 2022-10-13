94811304d5c5d1114fc32dd56565b8d5b1da71ed
Koichi Kairiyama <koichi.kairiyama@gmail.com>
2022-10-13 16:21:40 -0300

    Asdasd

--- app.js
+++ app.js
@@ -6,13 +6,27 @@ app.use(express.static(path.join(__dirname, 'public')))    /////     AGREGAR PUB
 
 app.listen(3070, () => {    // metodo listen de app para levantar el servidor : al ser un metodo necesita parametros : 
     // parametro1 : puerto donde lo queremos inicializar , // parametro2 : callback function 
-        console.log('Servidor corriendo')
+        console.log('Servidor corriendoooo')
     });
-    
 
 app.get('/', (req, res) => {   // parametro1 ('/') : pad,   // parametro2 : callback que obtiene como argumentos dos argumentos (req,res)
     res.sendFile(path.join(__dirname, '/views/index.html') )
 }); 
+
+app.get('/register', (req, res) => {   // parametro1 ('/') : pad,   // parametro2 : callback que obtiene como argumentos dos argumentos (req,res)
+    res.sendFile(path.join(__dirname, '/views/register.html') )
+}); 
+
+
+app.get('/login', (req, res) => {   // parametro1 ('/') : pad,   // parametro2 : callback que obtiene como argumentos dos argumentos (req,res)
+    res.sendFile(path.join(__dirname, '/views/login.html') )
+}); 
+
+
+
+
+
+
 
 
 
