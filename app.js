2f78f0c0717639c69b6e8022cf9b47c17898586d
Koichi Kairiyama <koichi.kairiyama@gmail.com>
2022-10-13 16:49:15 -0300

    asda

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))    /////     AGREGAR PUBLIC
 
-app.listen(3070, () => {    // metodo listen de app para levantar el servidor : al ser un metodo necesita parametros : 
-    // parametro1 : puerto donde lo queremos inicializar , // parametro2 : callback function 
-        console.log('Servidor corriendoooo')
-    });
+app.listen(process.env.PORT || 3070, () => console.log('El servidor esta corriendo en el puerto 3070.'));
 
 app.get('/', (req, res) => {   // parametro1 ('/') : pad,   // parametro2 : callback que obtiene como argumentos dos argumentos (req,res)
     res.sendFile(path.join(__dirname, '/views/index.html') )
